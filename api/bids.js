c971c459aa50eef00e3b2b7562b7d20f9a2b8a0b
pudders <ericpudney@gmail.com>
2024-04-16 16:33:52 +0200

    added bids endpoints

--- api/bids.js
+++ api/bids.js
@@ -1,14 +1,43 @@
 import bidModel from "../models/bidModel.js";
 
 export default function bids(server) {
+  server.get("/api/bids", async (req, res) => {
+    const bids = await bidModel.find();
+        res.json(bids);
+  })
+
+  server.get("/api/bids/:id", async (req, res) => {
+    try { 
+      if (req.body.user) {
+      const bids = await bidModel.find({bidder: id});
+      res.json(bids);
+    }
+    else if (req.body.item) {
+      const bids = await bidModel.find({item: id});
+      res.json(bids);
+    }
+    else {
+      res.status(404).json({message: 'The request does not specify whether bids are required for a user or an item.'})
+    }
+    }
+    catch {
+      res.status(500).json({ message: err.message });
+    }
+  })
+
   server.post("/api/bids", async (req, res) => {
     const bid = new bidModel({
       amount: req.body.amount,
-      bidder: session.login,
+      bidder: req.session.login,
       item: req.body.item,
       date: new Date(),
     });
-    const result = await bid.save();
-    console.log(result);
-  });
+      try {
+        const result = await bid.save();
+        res.status(201).json({message: 'Bid successfully placed.'});
+      }
+      catch (err) {
+        res.status(500).json({ message: err.message });
+      }
+    });
 }
