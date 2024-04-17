3d82aed6c4c8732793f0c9423d8395170b0afbef
pudders <ericpudney@gmail.com>
2024-04-17 09:14:23 +0200

    fixed and tested bidlist for users/objects

--- api/bids.js
+++ api/bids.js
@@ -8,20 +8,23 @@ export default function bids(server) {
 
   server.get("/api/bids/:id", async (req, res) => {
     try { 
-      if (req.body.user) {
-      const bids = await bidModel.find({bidder: id});
-      res.json(bids);
+      const userBids = await bidModel.find({bidder: req.params.id});
+      const objectBids = await bidModel.find({item: req.params.id});
+      if (userBids.length > 0 && objectBids.length > 0) {
+        return res.status(500).json({message: 'Found both a user and an object with this ID. ???'})
+      }
+      else if (userBids.length > 0) {
+        return res.status(200).json({message: 'returning bids for user ' + req.params.id, userBids: userBids});
+      }
+      else if (objectBids.length > 0) {
+        return res.status(200).json({message: 'returning bids on object ' + req.params.id, objectBids: objectBids});
+      }
+      else {
+        return res.status(404).json({message: 'No user or item with that ID found.'})
     }
-    else if (req.body.item) {
-      const bids = await bidModel.find({item: id});
-      res.json(bids);
     }
-    else {
-      res.status(404).json({message: 'The request does not specify whether bids are required for a user or an item.'})
-    }
-    }
-    catch {
-      res.status(500).json({ message: err.message });
+    catch (err) {
+      return res.status(500).json({ message: err.message });
     }
   })
 
